fef6c2a0d68db5916d8945e18023e2affb6d2ae0
Toli Carter <toli@playstudios.com>
2015-11-02 16:01:30 -0800

    Navigation manager written, not fully implemented.

---
+++ Assets/Scripts/NavigationManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class NavigationManager : MonoBehaviour
+{
+    private bool _CanNavigate;
+
+    public static Dictionary<string, string> RouteInformation = new Dictionary<string, string>()
+    {
+        {"World", "The big wild world." },
+        {"Cave", "The deep dark cave." },
+        {"City", "The bustling city" },
+        {"Dungeon", "The deep dark dungeon" },
+        {"Lair", "The hoard of your treasures" }
+    };
+
+    public static string GetRouteInfo(string destination)
+    {
+        return RouteInformation.ContainsKey(destination) ? RouteInformation[destination] : null;
+    }
+     public static bool CanNavigate(string destination)
+    {
+        return true;
+    }
+    public static void NavigateTo(string destination)
+    {
+    //Application.LoadLevel(destination)
+    }
+}
