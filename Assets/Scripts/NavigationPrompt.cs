18d7165e1dcd7250eb82942dac4d2c0f87351e42
Toli Carter <drachis@gmail.com>
2016-01-16 18:29:53 -0800

    extended game with UI for mobile, added Lair level

--- Assets/Scripts/NavigationPrompt.cs
+++ Assets/Scripts/NavigationPrompt.cs
@@ -5,6 +5,16 @@ public class NavigationPrompt : MonoBehaviour {
 
     bool showDialog;
 
+    Vector2 ratio = new Vector2((float)Screen.width/800, (float)Screen.height/600);
+    GUIStyle fontStyle;
+    public int fScale = 30;
+    void Start()
+    {
+
+        fontStyle = new GUIStyle();
+        fontStyle.fontSize = fScale * Screen.width/800;
+        fontStyle.normal.textColor = Color.gray;
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         showDialog = true;
@@ -19,19 +29,19 @@ public class NavigationPrompt : MonoBehaviour {
     {
         if (showDialog)
         {
-            GUI.BeginGroup(new Rect(Screen.width / 2 - 150, 50, 300, 250));
+            GUI.BeginGroup(new Rect(Screen.width / 2 - 150*ratio.x, 50*ratio.y, 300*ratio.x, 250*ratio.y));
 
-            GUI.Box(new Rect(0, 0, 300, 250), "");
+            GUI.Box(new Rect(0, 0, 300*ratio.x, 200*ratio.y), "");
 
-            GUI.Label(new Rect(15, 10, 300, 68), "Do you want to travel to " + this.tag + " ?");
+            GUI.Label(new Rect(15*ratio.x, 10*ratio.y, 300*ratio.x, 68*ratio.y), "Do you want to travel\nto the " + this.tag + " ?", fontStyle);
 
-            if ( GUI.Button(new Rect(55, 100, 180, 40), "Travel"))
+            if ( GUI.Button(new Rect(55*ratio.x, 100*ratio.y, 180*ratio.x, 40*ratio.y), "Travel", fontStyle))
             {
                 showDialog = false;
-                //Application.LoadLevel(1);
+                UnityEngine.SceneManagement.SceneManager.LoadScene(this.tag);
             }
 
-            if (GUI.Button(new Rect(55, 150, 180, 40), "Stay"))
+            if (GUI.Button(new Rect(55*ratio.x, 150*ratio.y, 180*ratio.x, 40*ratio.y), "Stay", fontStyle))
             {
                 showDialog = false;
             }
